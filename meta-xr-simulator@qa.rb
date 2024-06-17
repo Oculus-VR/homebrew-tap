c50860dce846a69e3c3074af8aa56e9bcac7b373
Xiang Wei <xiang.wei@oculus.com>
2024-06-17 10:24:40 -0700

    Create meta-xr-simulator@qa.rb

---
+++ meta-xr-simulator@qa.rb
@@ -0,0 +1,45 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+class MetaXrSimulator < Formula
+  desc "Meta XR Simulator"
+  homepage "https://developer.oculus.com/downloads/package/meta-xr-simulator/"
+  url "https://github.com/Oculus-VR/homebrew-repo/raw/main/repo/meta-xr-simulator/meta-xr-simulator-67.0.0-alpha.1.tar.gz"
+  sha256 "449e2bfbdf23f4d36f89ea47c90b917e0fffc49578fb704db571e948e6ca103e"
+  license "Meta Platform Technologies SDK"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    prefix.install Dir["*"]
+    puts
+    opoo "By installing this software, you agree to Meta Platform Technologies SDK License, which can be found at: https://developer.oculus.com/licenses/oculussdk/"
+    opoo "More details for setting up OpenXR on macOS can be found at https://github.com/Oculus-VR/homebrew-repo/blob/main/meta-xr-simulator.md"
+    puts
+    unless File.exist?("/usr/local/lib/libvulkan.1.dylib") && File.exist?("/usr/local/lib/libMoltenVK.dylib")
+      opoo "\u{1F6D1} Vulkan SDK and/or MoltenVK not found in /usr/local/lib \u{1F6D1}"
+      opoo "Please follow the instruction on https://vulkan.lunarg.com/doc/sdk/latest/mac/getting_started.html, using using 'System Global Installation'."
+      puts
+    end
+    opoo "\u{1F950} Please run the following command to activate Meta XR Simulator \u{1F950}"
+    opoo "sudo #{prefix}/post_installation_macos.sh"
+    puts
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test meta-xr-simulator`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
